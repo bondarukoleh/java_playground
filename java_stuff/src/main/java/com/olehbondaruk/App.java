83fd6c240c7a0415da46fcec499b85b52d017b9e
Oleh Bondaruk <bondaruk9000@gmail.com>
2020-04-28 23:41:40 +0200

    continue to lern maven basics

--- java_stuff/src/main/java/com/olehbondaruk/App.java
+++ java_stuff/src/main/java/com/olehbondaruk/App.java
@@ -1,10 +1,12 @@
 package com.olehbondaruk;
 
-/**
- * Hello world!
- */
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class App {
+    static Logger logger = LogManager.getLogger(App.class);
     public static void main(String[] args) {
         System.out.println("Ahh... shi... here we go again.");
+        logger.debug("Hello from logger!");
     }
 }

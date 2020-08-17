866fd78b86aa17cb14431e9dac7b5f334357a3df
Oleh Bondaruk <oleh.bondaruk@payconiq.com>
2020-08-17 17:47:59 +0200

    added waiter on java

---
+++ testing_stuff/src/waiter/WaitOptions.java
@@ -0,0 +1,6 @@
+public class WaitOptions {
+    public boolean throwError = false;
+    public int waitTime = 15000;
+    public int waitInterval = 1000;
+    public String errorMessage = "Unsuccessfully wait for expected condition";
+}

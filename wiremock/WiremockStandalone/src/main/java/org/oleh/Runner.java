5b653accfe7fd29f42ae44cda7a47b0cff6e90ec
Oleh Bondaruk <bondaruk9000@gmail.com>
2024-08-18 23:33:29 +0200

    added a bit about matching

--- wiremock/WiremockStandalone/src/main/java/org/oleh/Runner.java
+++ wiremock/WiremockStandalone/src/main/java/org/oleh/Runner.java
@@ -2,6 +2,7 @@ package org.oleh;
 
 import com.github.tomakehurst.wiremock.core.Options;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import java.io.IOException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,8 +10,6 @@ import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.cloud.contract.wiremock.WireMockSpring;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
-
-import java.io.IOException;
 
 @SpringBootApplication
 @AutoConfigureWireMock
@@ -23,9 +22,7 @@ public class Runner {
     @Bean
     public Options wireMockOptions(@Value("${wiremock.server.port}") int port, Environment env) throws IOException {
         final WireMockConfiguration options = WireMockSpring.options();
-        if(env.matchesProfiles("prod")) {
-            options.disableRequestJournal();
-        }
+        options.usingFilesUnderClasspath("target/classes");
         options.containerThreads(64);
         options.port(port);
         return options;
